bfd5f821ef807ed889ac8e5bf597d2c98ad04e58
Getaneh Tsegaye <getanehtsegaye@gmail.com>
2024-08-18 16:52:10 -0400

    created and connected the MongoDB

---
+++ README.md
@@ -0,0 +1,7 @@
+npm init -y
+use ECMA syntax
+npm i express
+npm i nodemon
+npm run start for node.js mode and npm run dev for nodemon mode.
+npm install dotenv for .env
+npm i mongoose for working on MongoDB
--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const app = express();
 
-app.listen(process.env.PORT, () => {
-  console.log(`App is listening on port ${process.env.PORT}`);
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("Welcome to Dubai Debremewi Admin Page");
 });
+
+mongoose
+  .connect(process.env.mongoDB_URL)
+  .then(() => {
+    console.log("App Connected to the Database");
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
